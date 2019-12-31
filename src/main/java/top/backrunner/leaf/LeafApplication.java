6f04bdf8235f63415172cb64928d043453f0f5cc
BackRunner <liaoyp126@126.com>
2019-12-31 17:44:52 +0800

    bug fix

--- src/main/java/top/backrunner/leaf/LeafApplication.java
+++ src/main/java/top/backrunner/leaf/LeafApplication.java
@@ -1,10 +1,24 @@
 package top.backrunner.leaf;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LeafApplication {
+
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        return new HttpMessageConverters(fastConverter);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LeafApplication.class, args);
